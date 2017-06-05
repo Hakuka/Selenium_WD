0a6006e7a715adad0e82ee11de6cdebd97b3ffe8
Sardukar <mmalec159@gmail.com>
2017-06-05 12:20:13 +0200

    Usunieto zbedne linie

--- src/poetrade/AnyCToEx.java
+++ src/poetrade/AnyCToEx.java
@@ -5,10 +5,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
-//meh - not needed
-//import org.openqa.selenium.support.ui.Select;
-//import org.openqa.selenium.WebElement;
-
 public class AnyCToEx {
 
 	public static void main(String[] args) throws InterruptedException {
